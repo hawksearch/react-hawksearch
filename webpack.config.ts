8afe15c5f5b3130745cb36e17c81590f725b9ea2
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-11-12 16:02:12 -0600

    Add html-webpack-plugin and HMR.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,32 @@
-import webpack from 'webpack';
+import * as webpack from 'webpack';
+
 import * as path from 'path';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: webpack.Configuration = {
 	entry: './src/index.ts',
 
 	output: {
 		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, './dist'),
 	},
 
 	devServer: {
 		contentBase: './dist',
+		hot: true,
+	},
+
+	resolve: {
+		// These are the reasonable defaults supported by the Node ecosystem.
+		// We also include JSX as a common component filename extension to support
+		// some tools, although we do not recommend using it, see:
+		// https://github.com/facebookincubator/create-react-app/issues/290
+		// `web` extension prefixes have been added for better support
+		// for React Native Web.
+		extensions: ['.mjs', '.web.ts', '.ts', '.web.tsx', '.tsx', '.web.js', '.js', '.json', '.web.jsx', '.jsx'],
+		alias: {
+			src: path.resolve(__dirname, './src'),
+		},
 	},
 
 	module: {
@@ -25,6 +41,14 @@ const config: webpack.Configuration = {
 	},
 
 	mode: 'development',
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			filename: 'index.html',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
 };
 
 export default config;
