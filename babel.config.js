bfb371655b19d5a5de447d3a8ce127c34ac7b68e
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-11-19 10:55:32 -0600

    Cleanup packages.

--- babel.config.js
+++ babel.config.js
@@ -24,7 +24,6 @@ module.exports = function(api) {
 		presets: presets,
 		plugins: [
 			'@babel/plugin-syntax-dynamic-import',
-			['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
 			'@babel/plugin-proposal-class-properties',
 			'@babel/plugin-proposal-object-rest-spread',
 			'react-hot-loader/babel',
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,80 +2,88 @@ import * as webpack from 'webpack';
 
 import * as path from 'path';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import BundleAnalyzerPlugin from 'webpack-bundle-analyzer';
 
-const config: webpack.Configuration = {
-	entry: './src/index.tsx',
+export default (env, argv): webpack.Configuration => {
+	let isDevBuild = true;
 
-	output: {
-		filename: '[name].js',
-		chunkFilename: '[name].[contentHash:8].js',
-		path: path.resolve(__dirname, './dist'),
-	},
+	if (argv && argv.mode === 'production') {
+		isDevBuild = false;
+	}
 
-	devServer: {
-		contentBase: './dist',
-		hot: true,
-	},
+	return {
+		entry: './src/index.tsx',
 
-	// TODO: 'source-map' in prod
-	devtool: 'eval-source-map',
-
-	resolve: {
-		// These are the reasonable defaults supported by the Node ecosystem.
-		// We also include JSX as a common component filename extension to support
-		// some tools, although we do not recommend using it, see:
-		// https://github.com/facebookincubator/create-react-app/issues/290
-		// `web` extension prefixes have been added for better support
-		// for React Native Web.
-		extensions: ['.mjs', '.web.ts', '.ts', '.web.tsx', '.tsx', '.web.js', '.js', '.json', '.web.jsx', '.jsx'],
-
-		modules: [
-			// adding ./src as a resolve path to match what's configured in tsconfig.json
-			path.resolve(__dirname, './src'),
-			path.resolve(__dirname, './node_modules'),
-		],
-	},
-
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendor',
-					priority: -20,
-					chunks: 'all',
-				},
-			},
+		output: {
+			filename: '[name].js',
+			chunkFilename: '[name].[contentHash:8].js',
+			path: isDevBuild ? undefined : path.resolve(__dirname, './dist'),
 		},
-		runtimeChunk: 'single',
-	},
 
-	module: {
-		rules: [
-			{
-				// feed typescript and javascript through babel
-				test: /\.(tsx?)|\.(jsx?)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
+		devServer: {
+			contentBase: './dist',
+			hot: true,
+		},
+
+		devtool: isDevBuild ? 'eval-source-map' : 'source-map',
+
+		resolve: {
+			// These are the reasonable defaults supported by the Node ecosystem.
+			// We also include JSX as a common component filename extension to support
+			// some tools, although we do not recommend using it, see:
+			// https://github.com/facebookincubator/create-react-app/issues/290
+			// `web` extension prefixes have been added for better support
+			// for React Native Web.
+			extensions: ['.mjs', '.web.ts', '.ts', '.web.tsx', '.tsx', '.web.js', '.js', '.json', '.web.jsx', '.jsx'],
+
+			modules: [
+				// adding ./src as a resolve path to match what's configured in tsconfig.json
+				path.resolve(__dirname, './src'),
+				path.resolve(__dirname, './node_modules'),
+			],
+		},
+
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					vendor: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendor',
+						priority: -20,
+						chunks: 'all',
 					},
 				},
 			},
+			runtimeChunk: 'single',
+		},
+
+		module: {
+			rules: [
+				{
+					// feed typescript and javascript through babel
+					test: /\.(tsx?)|\.(jsx?)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: true,
+						},
+					},
+				},
+			],
+		},
+
+		mode: isDevBuild ? 'development' : 'production',
+
+		plugins: [
+			new HtmlWebpackPlugin({
+				hash: true,
+				filename: 'index.html',
+				template: './src/index.html',
+			}),
+			new webpack.HotModuleReplacementPlugin(),
+			new BundleAnalyzerPlugin(),
 		],
-	},
-
-	mode: 'development',
-
-	plugins: [
-		new HtmlWebpackPlugin({
-			hash: true,
-			filename: 'index.html',
-			template: './src/index.html',
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-	],
+	};
 };
-
-export default config;
