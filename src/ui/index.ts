e7d86e1344e28ae52a1a23469cb2c2d95f9b7cff
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-12-05 17:28:57 -0600

    Remove unneeded file.

--- src/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Search } from './Search';
-export { default as Facets } from './Facets';
-export { default as Results } from './Results';
-export { default as SearchBox } from './SearchBox';
