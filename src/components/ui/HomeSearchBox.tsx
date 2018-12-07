5007453d602140665c2cf1153e8d0059f6e835c3
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-12-07 15:51:24 -0600

    Flesh out more of the autocomplete search box.

--- src/components/ui/HomeSearchBox.tsx
+++ src/components/ui/HomeSearchBox.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Downshift, { ControllerStateAndHelpers } from 'downshift';
 
-function HomeSearchBox() {
-	function getAutoSuggest() {}
+import { Response } from 'models/Autocomplete';
+import HawkClient from 'net/HawkClient';
 
+function HomeSearchBox() {
 	return (
 		<Downshift>
 			{(options: ControllerStateAndHelpers<{}>) => {
-				const { isOpen, getInputProps } = options;
+				const { isOpen, inputValue, getInputProps } = options;
 
 				return (
 					<div>
-						<input {...getInputProps()} />
-						<ul />
+						<input
+							{...getInputProps({
+								onKeyDown: event => {
+									if (event.key === 'Enter') {
+										console.log('should redirect to search.html?keyword=' + inputValue);
+
+										// TODO: configurable url
+										location.assign('search.html?keyword=' + inputValue);
+									}
+								},
+							})}
+						/>
+						{isOpen ? <SearchBoxData query={inputValue} /> : null}
 					</div>
 				);
 			}}
 		</Downshift>
 	);
+}
+
+function SearchBoxData({ query }) {
+	const client = new HawkClient();
+
+	const [results, setResults] = useState<Response>({});
+
+	async function getAutoSuggest(input: string) {
+		const response = await client.autocomplete({
+			ClientGuid: 'f51060e1c38446f0bacdf283390c37e8',
+			Keyword: input,
+		});
+
+		setResults(response);
+	}
+
+	useEffect(
+		() => {
+			const timeout = setTimeout(() => getAutoSuggest(query), 200);
+
+			return () => {
+				clearTimeout(timeout);
+			};
+		},
+		[query]
+	);
+
+	return <span>{JSON.stringify(results)}</span>;
 }
 
 export default HomeSearchBox;
--- src/net/HawkClient.ts
+++ src/net/HawkClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Request as SearchRequest, Response as SearchResponse } from 'models/Search';
+import { Request as AutocompleteRequest, Response as AutocompleteResponse } from 'models/Autocomplete';
 
 class HawkClient {
 	private baseUrl = 'https://searchapi-dev.hawksearch.net';
@@ -11,6 +12,17 @@ class HawkClient {
 			return result.data;
 		} catch (error) {
 			console.error('Search error:', error);
+			return null;
+		}
+	}
+
+	public async autocomplete(request: AutocompleteRequest): Promise<AutocompleteResponse | null> {
+		try {
+			const result = await axios.post<AutocompleteResponse>(this.baseUrl + '/api/autocomplete', request);
+
+			return result.data;
+		} catch (error) {
+			console.error('Autocomplete error:', error);
 			return null;
 		}
 	}
