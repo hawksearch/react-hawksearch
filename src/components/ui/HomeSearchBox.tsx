a38dae19c6e65f3fd417b180d8d477924753364d
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-12-11 14:17:48 -0600

    Override behavior of downshift.

--- src/components/ui/HomeSearchBox.tsx
+++ src/components/ui/HomeSearchBox.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import Downshift, { ControllerStateAndHelpers } from 'downshift';
+import Downshift, { ControllerStateAndHelpers, StateChangeOptions, DownshiftState } from 'downshift';
 
 import SearchSuggestions from './SearchSuggestions';
 import { useHawkConfig } from 'components/ConfigProvider';
+import { Product } from 'models/Autocomplete';
 
 function HomeSearchBox() {
 	const { config } = useHawkConfig();
 
 	const searchUrl = config.searchPageUrl || '/search';
 
+	function handleStateChange(
+		state: DownshiftState<Product>,
+		changes: StateChangeOptions<Product>
+	): Partial<StateChangeOptions<Product>> {
+		if (changes.type === Downshift.stateChangeTypes.mouseUp) {
+			// when the mouse is clicked outside of downshift, retain the input value that was typed in.
+			// by default downshift will clear the input value so we override this behavior here.
+			return { ...changes, inputValue: state.inputValue };
+		}
+
+		return changes;
 	}
 
 	return (
-		<Downshift>
-			{(options: ControllerStateAndHelpers<{}>) => {
-				const { isOpen, inputValue, getInputProps } = options;
+		<Downshift stateReducer={handleStateChange} itemToString={(item: Product) => item.ProductName}>
+			{(options: ControllerStateAndHelpers<Product>) => {
+				const { isOpen, inputValue, getInputProps, openMenu } = options;
 
 				const showSuggestions = isOpen && inputValue && inputValue.length > 0;
 				return (
@@ -31,6 +43,15 @@ function HomeSearchBox() {
 										location.assign(redirect);
 									}
 								},
+
+								// when the input is focused again, reopen the downshift menu
+								onFocus: () => {
+									if (inputValue && inputValue.length > 0) {
+										openMenu();
+									}
+								},
+
+								placeholder: 'Enter a search term',
 							})}
 						/>
 						{showSuggestions ? <SearchSuggestions query={inputValue || ''} downshift={options} /> : null}
