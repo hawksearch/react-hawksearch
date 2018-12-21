4448ca9948955c34f83c008f048c08dad693d601
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-12-21 16:51:05 -0600

    Add negation support to checkbox facets.

--- src/components/ui/Facets/Checkbox.tsx
+++ src/components/ui/Facets/Checkbox.tsx
@@ -2,16 +2,33 @@ import React, { useState } from 'react';
 
 import { useFacet } from './Facet';
 import { Value } from 'models/Facets';
+import { useHawkSearch } from 'components/StoreProvider';
 
 function Checkbox() {
-	const { facet, actor } = useFacet();
+	const { actor } = useHawkSearch();
+	const { facet } = useFacet();
 
 	const [filter, setFilter] = useState('');
 
 	function selectFacet(facetValue: Value) {
 		setFilter('');
-		actor.selectFacet(facetValue);
+		actor.selectFacet(facet, facetValue);
 	}
+
+	function negateFacet(facetValue: Value) {
+		setFilter('');
+		actor.selectFacet(facet, facetValue, /* negate */ true);
+	}
+
+	const filteredFacets = facet.Values.filter(val => {
+		if (!val.Label) {
+			// if a facet value doesn't have a label, we can't really filter down to it
+			// so exclude it
+			return false;
+		}
+
+		return val.Label.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+	});
 
 	return (
 		<div>
@@ -28,19 +45,11 @@ function Checkbox() {
 				</div>
 
 				<ul>
-					{facet.Values.filter(val => {
-						if (!val.Label) {
-							// if a facet value doesn't have a label, we can't really filter down to it
-							// so exclude it
-							return false;
-						}
-
-						return val.Label.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
-					}).map(value => (
+					{filteredFacets.map(value => (
 						<li key={value.Value}>
-							<button onClick={e => selectFacet(value)}>
-								{value.Selected ? '[x]' : null} {value.Label}
-							</button>
+							{value.Selected ? '[x]' : null}
+							<button onClick={e => selectFacet(value)}>{value.Label}</button>
+							<button onClick={e => negateFacet(value)}>X</button>
 						</li>
 					))}
 				</ul>
