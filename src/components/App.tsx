ce55b662284a1f23b45f4c7e4b5b5b4fa1e4d5a2
Ryan Stecker <ryan.stecker@americaneagle.com>
2018-12-28 19:00:36 -0600

    Show components sooner when loading.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 
-import { useHawkSearch } from 'components/StoreProvider';
 import SearchBox from 'components/ui/SearchBox';
 import { FacetRail } from 'components/ui/Facets';
 import { Results } from 'components/ui/Results';
 
 function App() {
-	const { store } = useHawkSearch();
-
 	return (
 		<div>
-			{store.isInitialLoad ? (
-				<div>
-					<span>Loading...</span>
-				</div>
-			) : (
-				<>
-					<SearchBox />
+			<SearchBox />
 
-					<FacetRail />
+			<FacetRail />
 
-					<Results />
-				</>
-			)}
+			<Results />
 		</div>
 	);
 }
