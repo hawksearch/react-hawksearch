a170bf61312f165c0ecfb7add67507c8f9bf83f0
alexanderteunis <alex.teunis@gmail.com>
2019-01-07 14:11:13 -0600

    Tablet facet styles.

--- src/components/ui/Results/ToolRow.tsx
+++ src/components/ui/Results/ToolRow.tsx
@@ -5,9 +5,13 @@ import { Sorting, Pagination } from './Tools';
 function ToolRow() {
 	return (
 		<div className="hawk-tool-row">
-			<Sorting />
+			<div className="hawk-tool-row__item">
+				<Sorting />
+			</div>
 
-			<Pagination />
+			<div className="hawk-tool-row__item">
+				<Pagination />
+			</div>
 		</div>
 	);
 }
