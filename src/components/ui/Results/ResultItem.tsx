c9eb81fbd9f79dc4595c1606c83d45aa34bc02cd
Ryan Stecker <ryan.stecker@americaneagle.com>
2019-01-23 14:28:05 -0600

    Alt text for result images.

--- src/components/ui/Results/ResultItem.tsx
+++ src/components/ui/Results/ResultItem.tsx
@@ -10,18 +10,24 @@ export interface ResultItemProps {
 function ResultItem({ item }: ResultItemProps) {
 	const [isLoaded, setIsLoaded] = useState(false);
 
+	const itemName = item.getDocumentValue('itemname');
+
 	return (
 		<div className="hawk-results__item">
 			<div className="hawk-results__item-image">
 				<div style={isLoaded ? {} : { overflow: 'hidden', width: '0px', height: '0px' }}>
-					<img onLoad={() => setIsLoaded(true)} src={item.getDocumentValue('image')} />
+					<img
+						onLoad={() => setIsLoaded(true)}
+						src={item.getDocumentValue('image')}
+						alt={`Image for ${itemName}`}
+					/>
 				</div>
 
 				{!isLoaded ? <PlaceholderImage /> : null}
 			</div>
 
 			<div className="hawk-results__item-name">
-				<span>{item.getDocumentValue('itemname')}</span>
+				<span>{itemName}</span>
 			</div>
 		</div>
 	);
