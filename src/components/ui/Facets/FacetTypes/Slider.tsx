f6a36d5ac5fa938cd0d25d32830c1cb2e414c3e7
lukasz.bargiel <lukasz.bargiel@americaneagle.com>
2019-04-12 08:23:16 -0500

    adding numeric inputs for facets

--- src/components/ui/Facets/FacetTypes/Slider.tsx
+++ src/components/ui/Facets/FacetTypes/Slider.tsx
@@ -3,11 +3,14 @@ import Rheostat, { PublicState } from 'rheostat';
 
 import { useHawkSearch } from 'components/StoreProvider';
 import { useFacet } from 'components/ui/Facets';
+import SliderNumericInputs from '../Controls/SliderNumericInputs';
 
-function Link() {
+function Slider() {
 	const {} = useHawkSearch();
+
 	const {
 		state: { facetValues },
+		facet,
 		actor,
 	} = useFacet();
 
@@ -41,13 +44,34 @@ function Link() {
 		actor.setFacets([selection]);
 	}
 
-	return (
+	function onMinChange(event: React.FormEvent<HTMLInputElement>) {
+		onNumericInputChange(false,event.currentTarget.value);
+	}
+
+	function onMaxChange(event: React.FormEvent<HTMLInputElement>) {		
+		onNumericInputChange(true,event.currentTarget.value);
+	}
+
+	function onNumericInputChange(isMax: boolean, value: string){		
+		isMax ? setMaxValue(parseFloat(value)) : setMinValue(parseFloat(value))
+
+		// this selection is sent to hawk separated by commas, so build the value here
+		const selection = `${minValue},${maxValue}`;
+
+		actor.setFacets([selection]);
+	}
+
+	return (		
 		<div className="hawk-facet-rail__facet-values">
 			<div className="hawk-facet-rail__facet-values-link">
+
+				<SliderNumericInputs min={rangeMin} max={rangeMax} values={[minValue, maxValue]} onMinChange={onMinChange} onMaxChange={onMaxChange} />				
 				<Rheostat min={rangeMin} max={rangeMax} values={[minValue, maxValue]} onChange={onChange} />
+				
+
 			</div>
 		</div>
 	);
 }
 
-export default Link;
+export default Slider;
