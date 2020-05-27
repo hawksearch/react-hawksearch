c4658214ef2406cb3bddad90101cdefe873836c0
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2020-05-27 11:28:26 +0300

    Update i18n.js

--- src/util/i18n.js
+++ src/util/i18n.js
@@ -8,14 +8,12 @@ const resources = {};
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
-        resources,
-        lng: "en",
-
-        keySeparator: false, // we do not use keys in form messages.welcome
-
         interpolation: {
             escapeValue: false // react already safes from xss
-        }
+        },
+        keySeparator: false, // we do not use keys in form messages.welcome
+        lng: "en",
+        resources
     });
 
 export default i18n;
