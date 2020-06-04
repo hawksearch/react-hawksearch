50e826803778cfe8bc5ef21dfa8ce387e0625292
Michael Yankelev <michael.yankelev@hawksearch.com>
2020-06-04 11:28:43 -0500

    HSP2020-915 Resolve tag facet in facet rail

--- src/models/Facets/Facet.ts
+++ src/models/Facets/Facet.ts
@@ -13,7 +13,7 @@ export class Facet {
 	/** The name of the field that is linked to this facet. */
 	public Field: string;
 
-	public FieldType: 'string' | 'range' | 'numeric' | 'search';
+	public FieldType: 'string' | 'range' | 'numeric' | 'search' | 'tag';
 	public FacetType: FacetType;
 
 	public DisplayType: 'default' | 'scrolling' | 'truncating';
