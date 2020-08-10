38fd7d45115c99c2f91ddfb34e436ae6948f1047
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2020-08-10 01:58:57 -0500

    Revert "Merged PR 150: Package for Azure
    https://hawksearch.atlassian.net/browse/HS20-1319"

--- src/models/Search/Merchandising.ts
+++ src/models/Search/Merchandising.ts
@@ -28,7 +28,6 @@ export class PageContentItem {
 	public Zone: string;
 	public ContentType: ContentType;
 	public ImageUrl: string;
-	public ImageTitle: string;
 	public AltTag: string;
 	public ForwardUrl: string;
 	public Output: any;
