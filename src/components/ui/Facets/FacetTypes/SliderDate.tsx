1e99d0f236c598e3fdb22b25211e885809c5ba05
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2020-09-02 08:47:09 -0500

    HSP2020-1432 Added missing tracking events on sliderdate and slidernumeric

--- src/components/ui/Facets/FacetTypes/SliderDate.tsx
+++ src/components/ui/Facets/FacetTypes/SliderDate.tsx
@@ -4,6 +4,7 @@ import { PublicState } from 'rheostat';
 import { useHawksearch } from 'components/StoreProvider';
 import { useFacet } from 'components/ui/Facets/Facet';
 import SliderCalendarInputs from '../SliderCalendarInputs';
+import TrackingEvent from 'components/TrackingEvent';
 const Rheostat = React.lazy(() => import(/* webpackChunkName: "rheostat" */ 'rheostat'));
 
 function formatDate(date: Date) {
@@ -26,7 +27,7 @@ function getTime(date) {
 
 function SliderDate() {
 	const {
-		store: { facetSelections },
+		store: { facetSelections, searchResults },
 	} = useHawksearch();
 
 	const {
@@ -141,6 +142,10 @@ function SliderDate() {
 
 		// this selection is sent to hawk separated by commas, so build the value here
 		const selection = `${formattedMinVal},${formattedMaxVal}`;
+		TrackingEvent.track('searchtracking', {
+			trackingId: searchResults ? searchResults.TrackingId : '',
+			typeId: 2,
+		});
 		actor.setFacets([selection]);
 	}
 
