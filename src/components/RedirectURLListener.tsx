5fbe3fe64eaaa7c5ce6b1aca0ffe4dfd1371a155
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2020-10-07 04:33:36 -0500

    HSP2020-1536-redirect-fix Added redirect fix from iframe

--- src/components/RedirectURLListener.tsx
+++ src/components/RedirectURLListener.tsx
@@ -5,7 +5,8 @@ function RedirectURLListener() {
 	const { store } = useHawksearch();
 	useEffect(() => {
 		if (store.searchResults && store.searchResults.Redirect.Location) {
-			window.location.href = store.searchResults.Redirect.Location;
+			// NOTE: This will redirect the parent window to the given URL
+			window.top.location.href = store.searchResults.Redirect.Location;
 		}
 	}, [store.searchResults]);
 
