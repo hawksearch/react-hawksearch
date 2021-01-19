b2e149dcf35004436118cd38d90a195ee3bb92e6
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2021-01-19 05:16:07 -0600

    HS2IP-5 Added adjusted keyword component

---
+++ src/components/ui/AdjustedKeyword.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useHawksearch } from 'components/StoreProvider';
+
+function AdjustedKeyword() {
+	const {
+		store: { searchResults },
+	} = useHawksearch();
+	console.log(searchResults);
+	if (searchResults && searchResults.AdjustedKeyword) {
+		return (
+			<div>
+				Showing results for <b>{searchResults.AdjustedKeyword}</b>. Search instead for{' '}
+				<b>{searchResults.Keyword}</b>.
+			</div>
+		);
+	}
+	return null;
+}
+
+export default AdjustedKeyword;
