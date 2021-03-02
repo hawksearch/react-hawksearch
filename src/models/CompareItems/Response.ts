9b3128761e2656310cecd3e09e6bf333db476e4d
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2021-03-02 07:22:29 -0600

    H21P-153 Refactor the response class for comparision feature

--- src/models/CompareItems/Response.ts
+++ src/models/CompareItems/Response.ts
@@ -1,22 +1,35 @@
-import { Result } from 'models/Search';
+export class CompareDataResult {
+	/** Unique identifier for this search result item. */
+	public DocId: string;
 
-export class CompareDataResult extends Result {
+	/** Calculated relevancy score. */
+	public Score: number;
+
+	/**
+	 * Contains the fields for the search result item, as an object of string keys to an array
+	 * of string values. The keys correspond to the name of the field within the hawk dashboard,
+	 * and the value of the map is an array of strings for each of the values for that field.
+	 */
+	public Document: { [field: string]: { value: string[]; compare: boolean } };
+
 	public getDocumentValueCompare(field: string): string | undefined {
 		if (this.Document) {
-			const values = this.Document[field];
+			const fieldObject = this.Document[field];
 
-			if (values && (values as any).value.length > 0) {
-				return (values as any).value[0];
+			if (fieldObject && fieldObject.value.length > 0) {
+				return fieldObject.value[0];
 			}
 		}
 
 		return undefined;
 	}
+
 	public constructor(init: CompareDataResult) {
-		super(init);
+		Object.assign(this, init);
 	}
 }
-export class Response extends Result {
+
+export class Response extends CompareDataResult {
 	public Results: CompareDataResult[];
 
 	public constructor(init: Response) {
