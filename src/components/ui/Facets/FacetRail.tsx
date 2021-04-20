8a72e32ab3026789099e80f3fd12d4905b67419f
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2021-04-20 17:45:15 +0300

    Reverted window resize custom function

--- src/components/ui/Facets/FacetRail.tsx
+++ src/components/ui/Facets/FacetRail.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useWindowSize } from '@react-hook/window-size';
+//import { useWindowSize } from '@react-hook/window-size';
+import { useWindowSize } from 'util/WindowResize';
 
 import { useTranslation } from 'react-i18next';
 import FacetList from './FacetList';
@@ -7,12 +8,14 @@ import FacetList from './FacetList';
 function FacetRail() {
 	const { t, i18n } = useTranslation();
 	const [isCollapsed, setCollapsed] = useState(false);
-	const [width] = useWindowSize();
+	//const [width] = useWindowSize();
+	const size = useWindowSize();
+
 	return (
 		<div className="hawk-facet-rail">
 			<div
 				className="hawk-facet-rail__heading"
-				{...(width <= 767 && { onClick: () => setCollapsed(!isCollapsed) })}
+				{...(size.width <= 767 && { onClick: () => setCollapsed(!isCollapsed) })}
 			>
 				{t('Filter By')}
 			</div>
