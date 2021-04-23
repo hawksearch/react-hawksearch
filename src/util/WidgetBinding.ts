d5d0f958c8f0cc0adf803c182195872c7b673892
Gabriel Georgiev <10907836+GabrielGeorgiev@users.noreply.github.com>
2021-04-23 12:00:55 +0300

    Add comments

--- src/util/WidgetBinding.ts
+++ src/util/WidgetBinding.ts
@@ -1,6 +1,18 @@
+/**
+ * Object containing all initialized stores
+ * */
 const dataLayers = {}
+
+/**
+ * All initialized configurations
+ * Used for index name verification
+ * */
 const configurations = {}
 
+/**
+ * A method invoked at store updates that propagates the data between similar stores
+ * Store matching is based on the unique identifier and the configuration index name
+ */
 export function updateBindedStores({ dataLayer, widgetId, store, actor, config }) {
 	if (!dataLayers.hasOwnProperty(dataLayer)) {
 		dataLayers[dataLayer] = {};
