15df5bd42d95f8f76f113b162f1ccb58af9b2fc8
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2021-05-10 02:57:06 -0500

    H2IP-1137 Code review changes

--- src/components/ui/RelatedSearch.tsx
+++ src/components/ui/RelatedSearch.tsx
@@ -7,33 +7,27 @@ function RelatedSearch() {
 		store: { searchResults },
 	} = useHawksearch();
 	const relatedFacet = searchResults && searchResults.Facets.find(facet => facet.FacetType === 'related');
-	console.log(relatedFacet);
 
 	function searchWithKeyword(keyword) {
 		hawkActor.setSearch({
 			Keyword: keyword,
 		});
 	}
+
+	if (!relatedFacet) {
+		return null;
+	}
 	return (
 		<>
-			{relatedFacet && (
-				<div className="hawk-related_search-container">
-					<span className="heading">Related Searches: </span>
-					{relatedFacet.Values.map((item, index) => {
-						console.log(index, relatedFacet.Values.length);
-						return (
-							<span
-								key={index}
-								onClick={() => searchWithKeyword(item.Value)}
-								className="related-searched-words"
-							>
-								{' '}
-								{item.Label} {index < relatedFacet.Values.length - 1 && <>|</>}
-							</span>
-						);
-					})}
-				</div>
-			)}
+			<div className="hawk-related_search-container">
+				<span className="heading">Related Searches: </span>
+				{relatedFacet.Values.map((item, index) => (
+					<span key={index} onClick={() => searchWithKeyword(item.Value)} className="related-searched-words">
+						{' '}
+						{item.Label} {index < relatedFacet.Values.length - 1 && <>|</>}
+					</span>
+				))}
+			</div>
 		</>
 	);
 }
