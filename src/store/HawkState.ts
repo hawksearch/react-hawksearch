7970ac73a6acb495b21e312db1249ba547b33871
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2021-07-02 04:42:51 -0500

    HS-50 Fixed clearing visitor target issue from smartbar preview

--- src/store/HawkState.ts
+++ src/store/HawkState.ts
@@ -120,6 +120,8 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 			comparedResults: [],
 			itemsToCompareIds: [],
 			productDetails: {},
+			language: getInitialLanguage(),
+			smartBar: {},
 		}),
 		SearchStore
 	);
@@ -153,6 +155,7 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 			IsInPreview: config.isInPreview,
 			// and override some of the request fields with config values
 			ClientGuid: config.clientGuid,
+			ClientData: getClientData(),
 			Keyword: store.pendingSearch.Keyword
 				? decodeURIComponent(store.pendingSearch.Keyword || '')
 				: store.pendingSearch.Keyword,
@@ -171,29 +174,9 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 			setStore({ isLoading: false });
 			return;
 		}
-		// Fill clientdata
-		let visitId = getCookie('hawk_visit_id');
-		let visitorId = getCookie('hawk_visitor_id');
-		if (!visitId) {
-			setCookie('hawk_visit_id', createGuid(), getVisitExpiry());
-			visitId = getCookie('hawk_visit_id');
-		}
-		if (!visitorId) {
-			setCookie('hawk_visitor_id', createGuid(), getVisitorExpiry());
-			visitorId = getCookie('hawk_visitor_id');
-		}
-		const updatedRequest = {
-			ClientData: {
-				VisitorId: visitorId || '',
-				VisitId: visitId || '',
-				UserAgent: navigator.userAgent,
-				PreviewBuckets: store.searchResults ? store.searchResults.VisitorTargets.map(v => v.Id) : [],
-			},
-			...searchParams,
-		};
 
 		try {
-			searchResults = await client.search(updatedRequest, cancellationToken);
+			searchResults = await client.search(searchParams as any, cancellationToken);
 		} catch (error) {
 			if (axios.isCancel(error)) {
 				// if the request was cancelled, it's because this component was updated
@@ -542,6 +525,52 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 			itemsToCompare: [],
 			itemsToCompareIds: [],
 		});
+	}
+
+	function getClientData() {
+		let visitId = getCookie('hawk_visit_id');
+		let visitorId = getCookie('hawk_visitor_id');
+
+		if (!visitId) {
+			setCookie('hawk_visit_id', createGuid(), getVisitExpiry());
+			visitId = getCookie('hawk_visit_id');
+		}
+
+		if (!visitorId) {
+			setCookie('hawk_visitor_id', createGuid(), getVisitorExpiry());
+			visitorId = getCookie('hawk_visitor_id');
+		}
+
+		let clientData: ClientData = {
+			VisitorId: visitorId || '',
+			VisitId: visitId || '',
+			UserAgent: navigator.userAgent,
+			PreviewBuckets:
+				store.searchResults && !config.disablePreviewBuckets
+					? store.searchResults.VisitorTargets.map(v => v.Id)
+					: [],
+		};
+		if (store.pendingSearch.ClientData) {
+			clientData = {
+				...clientData,
+				PreviewBuckets: store.pendingSearch.ClientData.PreviewBuckets,
+			};
+		}
+		const language = store.language;
+
+		if (language) {
+			clientData.Custom = {
+				language,
+			};
+		}
+
+		return clientData;
+	}
+
+	function getInitialLanguage() {
+		const urlParams = new URLSearchParams(location.search);
+		const language = urlParams.get('language') || config.language;
+		return language;
 	}
 
 	const actor: SearchActor = {
