ebd5941a20e08a99bd59bcca5e169a7aad354c7c
Abdullah Waqas <abdullah.waqas@americaneagle.com>
2021-07-05 23:24:35 -0500

    HS-202 Fixed preview date in smartbar payload while making search API

--- src/store/HawkState.ts
+++ src/store/HawkState.ts
@@ -170,7 +170,7 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 			SearchWithin: store.pendingSearch.SearchWithin
 				? decodeURIComponent(store.pendingSearch.SearchWithin || '')
 				: store.pendingSearch.SearchWithin,
-			SmartBar: store.smartBar || undefined,
+			SmartBar: store.pendingSearch.SmartBar,
 		};
 
 		// The index name in the configuration takes priority over the one supplied from the URL
@@ -593,6 +593,7 @@ export function useHawkState(initialSearch?: Partial<Request>): [SearchStore, Se
 					: [],
 		};
 		if (store.pendingSearch.ClientData) {
+			console.log(store.pendingSearch);
 			clientData = {
 				...clientData,
 				PreviewBuckets: store.pendingSearch.ClientData.PreviewBuckets,
